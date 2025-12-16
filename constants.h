69d88847a607f1b3836c234e6b933c436d55a1cb
Nurdin Hossain <nurdinhossain@Nurdins-MacBook-Air.local>
2025-12-15 19:11:38 -0500

    Add constants for chess board square and piece color

---
+++ constants.h
@@ -0,0 +1,78 @@
+#pragma once
+
+// colors
+#define WHITE 0
+#define BLACK 1
+
+// chess squares
+#define h1 0
+#define g1 1
+#define f1 2
+#define e1 3
+#define d1 4
+#define c1 5
+#define b1 6
+#define a1 7
+
+#define h2 8
+#define g2 9
+#define f2 10
+#define e2 11
+#define d2 12
+#define c2 13
+#define b2 14
+#define a2 15
+
+#define h3 16
+#define g3 17
+#define f3 18
+#define e3 19
+#define d3 20
+#define c3 21
+#define b3 22
+#define a3 23
+
+#define h4 24
+#define g4 25
+#define f4 26
+#define e4 27
+#define d4 28
+#define c4 29
+#define b4 30
+#define a4 31
+
+#define h5 32
+#define g5 33
+#define f5 34
+#define e5 35
+#define d5 36
+#define c5 37
+#define b5 38
+#define a5 39
+
+#define h6 40
+#define g6 41
+#define f6 42
+#define e6 43
+#define d6 44
+#define c6 45
+#define b6 46
+#define a6 47
+
+#define h7 48
+#define g7 49
+#define f7 50
+#define e7 51
+#define d7 52
+#define c7 53
+#define b7 54
+#define a7 55
+
+#define h8 56
+#define g8 57
+#define f8 58
+#define e8 59
+#define d8 60
+#define c8 61
+#define b8 62
+#define a8 63
