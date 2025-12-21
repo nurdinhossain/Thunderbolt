1efb8117de431e5a49d24ac67b05c652e2f4d85a
Nurdin Hossain <nurdinhossain@macbookair.mynetworksettings.com>
2025-12-21 00:01:47 -0500

    Implement xor-shift random number generator

--- sliding.cpp
+++ sliding.cpp
@@ -10,6 +10,19 @@ u64 rook_magics[NUM_SQUARES] = {0};
 u64 bishop_attacks[NUM_SQUARES][1ULL << BISHOP_MAGIC_BITS] = {0};
 u64 rook_attacks[NUM_SQUARES][1ULL << ROOK_MAGIC_BITS] = {0};
 
+// seed for rng
+u64 seed = 1;
+
+// XOR-shift function for random number generation
+u64 rng()
+{
+    seed ^= (seed << 5);
+    seed ^= (seed << 17);
+    seed ^= (seed >> 3);
+    seed ^= (seed << 11);
+    return seed;
+}
+
 // functions to generate magics
 void generate_bishop_magics()
 {
@@ -39,9 +52,35 @@ void generate_bishop_magics()
         }
         full_attack_mask &= ~(file_masks[file_a] | file_masks[file_h] | rank_masks[rank_1] | rank_masks[rank_8]);
 
-        // collect all possible blocker bitboards for this square
-        vector<u64> blockers;
-        display(full_attack_mask);
+        // collect every index that a blocker could occupy
+        vector<int> potential_blocker_indices;
+
+        while (full_attack_mask > 0)
+        {
+            int index = lsb(full_attack_mask);
+            potential_blocker_indices.push_back(index);
+            full_attack_mask &= (full_attack_mask - 1);
+        }
+
+        // use potential_blocker_indices to find all possible blocker boards
+        int chosen = 0;
+        vector<u64> blockers; 
+        while (chosen < (1ULL << potential_blocker_indices.size()))
+        {
+            int temp_chosen = chosen;
+            u64 blocker_mask = 0ULL;
+
+            while (temp_chosen > 0)
+            {
+                blocker_mask ^= (1ULL << potential_blocker_indices[lsb(temp_chosen)]);
+                temp_chosen &= (temp_chosen - 1);
+            }
+
+            blockers.push_back(blocker_mask);
+            chosen++;
+        }
+
+        // 
     }
 }
 
@@ -51,5 +90,11 @@ int main()
 {
     generate_static_bitboards();
     generate_bishop_magics();
+
+    for (int i = 0; i < 5000; i++)
+    {
+        cout << (rng() >> (64 - 5)) << ", ";
+    }
+    cout << endl;
     return 0;
 }
--- sliding.h
+++ sliding.h
@@ -14,6 +14,12 @@ extern u64 rook_magics[NUM_SQUARES];
 extern u64 bishop_attacks[NUM_SQUARES][1ULL << BISHOP_MAGIC_BITS];
 extern u64 rook_attacks[NUM_SQUARES][1ULL << ROOK_MAGIC_BITS];
 
+// seed number for rng
+extern u64 seed;
+
+// function to generate random number
+u64 rng();
+
 // functions to generate magics
 void generate_bishop_magics();
 void generate_rook_magics();
