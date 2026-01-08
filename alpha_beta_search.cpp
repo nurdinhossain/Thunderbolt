8b6988a61f59674562f9d74add692aaeb1de6cf7
Nurdin Hossain <nurdinhossain@macbookair.mynetworksettings.com>
2026-01-08 13:35:01 -0500

    Implement quiescence search

--- alpha_beta_search.cpp
+++ alpha_beta_search.cpp
@@ -1,6 +1,55 @@
 #include "alpha_beta_search.h"
 #include "moveorder.h"
 
+int AlphaBeta::quiesce(Board& board, int alpha, int beta)
+{
+    // get static eval 
+    int static_eval = Evaluate::eval(board);
+
+    // static eval = stand pat score
+    int best_value = static_eval;
+
+    if( best_value >= beta )
+        return best_value;
+    if( best_value > alpha )
+        alpha = best_value;
+
+    // examine all captures
+    MoveList moves;
+    board.generate_pseudo_legal_moves(moves);
+    order_moves(board, moves, move_order_flags);
+    
+    for (int i = 0; i < moves.count; i++)
+    {
+        Move m = moves.moves[i];
+
+        if (m.move_type == CAPTURE || m.move_type == EN_PASSANT_CAPTURE || m.move_type >= KNIGHT_PROMOTION_CAPTURE)
+        {
+            PreviousState prev = board.make_move(m);
+
+            // evaluate move if its legal
+            int score = -MAX_BOUND;
+            if ( !board.in_check( static_cast<Color>( 1-board.get_side_to_move() ) ) )
+            {
+                score = -quiesce(board, -beta, -alpha);
+            }
+
+            // undo move
+            board.unmake_move(m, prev);
+
+            // check for cutoffs
+            if( score >= beta )
+                return score;
+            if( score > best_value )
+                best_value = score;
+            if( score > alpha )
+                alpha = score;
+        }
+    }
+
+    return best_value;
+}
+
 int AlphaBeta::search(Board& board, int alpha, int beta, int depth, int ply)
 {
     // address basic draw conditions
@@ -16,7 +65,7 @@ int AlphaBeta::search(Board& board, int alpha, int beta, int depth, int ply)
     stats.nodes_searched++;
 
     // return static eval of position at leaf node
-    if (depth == 0) return Evaluate::eval(board);
+    if (depth == 0) return quiesce(board, alpha, beta);
 
     // generate pseudo legal moves
     int best_score = -MAX_BOUND;
--- alpha_beta_search.h
+++ alpha_beta_search.h
@@ -9,5 +9,6 @@ class AlphaBeta : public Negamax
         AlphaBeta() { move_order_flags = {true, true}; }
 
         // search
+        int quiesce(Board& board, int alpha, int beta);
         int search(Board& board, int alpha, int beta, int depth, int ply) override;
 };
