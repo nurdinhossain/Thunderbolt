1d9939abfb129076b84fce3d7366341825483ccc
Nurdin Hossain <nurdinhossain@macbookair.mynetworksettings.com>
2026-01-07 22:34:55 -0500

    Implement gauntlet result interpretability

--- gauntlet.cpp
+++ gauntlet.cpp
@@ -1,10 +1,11 @@
 #include "gauntlet.h"
 #include <iostream>
 
-int Gauntlet::fight(Search* ai_one, Search* ai_two, EvalParams eval_one, EvalParams eval_two, int rounds, int ms_per_move)
+pair<int, int> Gauntlet::fight(Search* ai_one, Search* ai_two, EvalParams eval_one, EvalParams eval_two, int rounds, int ms_per_move)
 {   
-    // record number of times ai_one wins
+    // record number of times ai_one wins/draws
     int ai_one_wins = 0;
+    int ai_one_draws = 0;
 
     // set time controls for both ai's
     ai_one->set_time_control(ms_per_move);
@@ -45,23 +46,33 @@ int Gauntlet::fight(Search* ai_one, Search* ai_two, EvalParams eval_one, EvalPar
         if (i % 2 == 0) // ai 1 is white
         {
             // black side is in check and can't move
-            if (board.in_check(BLACK)) ai_one_wins += 2;
+            if (board.in_check(BLACK)) ai_one_wins++;
 
             // white side is in check and can't move
             else if (board.in_check(WHITE)) ai_one_wins += 0;
 
             // neither side is in check (draw)
-            else ai_one_wins++;
+            else ai_one_draws++;
         } 
         else // ai 2 is white
         {
-            if (board.in_check(WHITE)) ai_one_wins += 2;
+            if (board.in_check(WHITE)) ai_one_wins++;
             else if (board.in_check(BLACK)) ai_one_wins += 0;
-            else ai_one_wins++;
+            else ai_one_draws++;
         }
         cout << "Round " << i + 1 << " complete!" << endl;
     }   
 
     // return ai one wins
-    return ai_one_wins;
+    return {ai_one_wins, ai_one_draws};
+}
+
+void Gauntlet::interpret_results(pair<int, int> wins_and_draws, int total_games)
+{
+    int wins = wins_and_draws.first;
+    int draws = wins_and_draws.second;
+    float score = (0.5 * draws + wins) / total_games;
+
+    cout << "AI 1 won " << wins << " times and drew " << draws << " times out of " << total_games << " rounds. ";
+    cout << "This is a score of " << score * 100 << "%." << endl;
 }
--- gauntlet.h
+++ gauntlet.h
@@ -5,5 +5,6 @@
 class Gauntlet
 {
     public:
-        static int fight(Search* ai_one, Search* ai_two, EvalParams eval_one, EvalParams eval_two, int rounds, int ms_per_move);
+        static pair<int ,int> fight(Search* ai_one, Search* ai_two, EvalParams eval_one, EvalParams eval_two, int rounds, int ms_per_move);
+        static void interpret_results(pair<int, int> wins_and_draws, int total_games);
 };
